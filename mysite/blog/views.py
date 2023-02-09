6c88f0e910b16f0d19cc9bed4128b9737176e17c
abuyahyadiab <abuyahyadiab@abuyahyadiabs-MacBook-Pro.local>
2023-02-09 22:44:11 +0200

    adding email forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 # from django.http import Http404
 
 
@@ -37,3 +38,30 @@ def post_detail(request, year, month, day, post):
                             publish__day = day,
                             status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+from .forms import EmailPostForm
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'abuyahyadiab@gmail.com',
+                      [cd['to']])
+            sent = True
+
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form, 'sent': sent})
